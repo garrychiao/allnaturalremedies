0d8f78da6de1eae637e68ed9d5e9ae5a02eda9fe
garrychiao <g50905g@gmail.com>
2016-10-12 15:43:41 +0800

    20161012 RWD Design done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,29 +93,48 @@
   <!--end navbar-->
   <div class="" id="fullpage">
     <div class="section" id="csshome" align="center">
-      <h1 class="animated pulse" id="title">Acupuncture & Massage Clinic Plus</h1>
-      <h2 class="animated pulse" id="content">Intergrated alternative therapies for body balance</h2>
-      <div class="col-sm-8 col-sm-offset-2">
-        <div class="col-sm-6">
-          <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> ACC Provider</h3>
-          <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Open 7 Days</h3>
+      <div class="hidden-xs">
+        <h1 class="animated pulse" id="title">Acupuncture & Massage Clinic Plus</h1>
+        <h2 class="animated pulse" id="content">Intergrated alternative therapies for body balance</h2>
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="col-sm-6">
+            <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> ACC Provider</h3>
+            <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Open 7 Days</h3>
+          </div>
+          <div class="col-sm-6">
+            <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Full Provision For Ladies</h3>
+            <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Female Therapist</h3>
+          </div>
+          <div class="col-sm-12" align="center">
+            <h3>Opening Hours :</h3>
+            <h3>Mon - Fri: 08:00 AM - 10:00 PM</h3>
+            <h3>Sat - Sun: 09:00 AM - 09:00 PM</h3>
+          </div>
         </div>
-        <div class="col-sm-6">
-          <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Full Provision For Ladies</h3>
-          <h3><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Female Therapist</h3>
-        </div>
-        <div class="col-sm-12" align="center">
-          <h3>Opening Hours :</h3>
-          <h3>Mon - Fri: 08:00 AM - 10:00 PM</h3>
-          <h3>Sat - Sun: 09:00 AM - 09:00 PM</h3>
+      </div>
+      <div class="visible-xs">
+        <h2 class="animated pulse" id="title">Acupuncture & Massage Clinic Plus</h2>
+        <h4 class="animated pulse" id="content">Intergrated alternative therapies for body balance</h4>
+        <div class="col-sm-8 col-sm-offset-2">
+          <div class="col-xs-12">
+            <h4><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> ACC Provider</h4>
+            <h4><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Open 7 Days</h4>
+            <h4><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Full Provision For Ladies</h4>
+            <h4><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Female Therapist</h4>
+          </div>
+          <div class="col-xs-12" align="center">
+            <h4>Opening Hours :</h4>
+            <h4>Mon - Fri: 08:00 AM - 10:00 PM</h4>
+            <h4>Sat - Sun: 09:00 AM - 09:00 PM</h4>
+          </div>
         </div>
       </div>
     </div>
     <div class="section" id="csstherapies" align="center">
       <!--2*2 per slide-->
       <div class="slide">
-        <h1 id="title">Therapies Provided:</h1>
         <div class="col-sm-12 hidden-xs">
+          <h1 id="title">Therapies Provided:</h1>
           <div class="col-sm-5 col-sm-offset-1">
             <!--A part of introduction section-->
             <!--the first col-->
@@ -180,16 +199,72 @@
             </div>
           </div>
         </div>
+        <div class="visible-xs">
+          <h2 id="title">Therapies Provided:</h2>
+          <div class="col-xs-12">
+            <div class="col-xs-12">
+              <li><h4 id="content">Acupuncture</h4></li>
+              <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                  <img src="img/img_acupuncture.jpeg" alt="...">
+                </a>
+              </div>
+              <div class="col-xs-8">
+                <p>
+                  Hundreds of clinical studies on the benefits of acupuncture show that it successfully treats conditions ranging from musculoskeletal problems (back pain, neck pain, and others) to nausea, migraine headache, anxiety, depression, insomnia, infertility, etc.
+                </p>
+              </div>
+            </div>
+            <div class="col-xs-12">
+              <li><h4 id="content">​Nutrition & Herbal</h4></li>
+              <div class="col-xs-4">
+                <a href="#" class="thumbnail">
+                  <img src="img/img_herbal.jpg" alt="...">
+                </a>
+              </div>
+              <div class="col-xs-8">
+                <p>
+                  We are the retailers of New Zealand mede nutrition powders and herbal suppliments.  Herbal suppliments are without side effects- all trusted brands at a very low price.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="slide">
-        <h1 id="title">Something Here</h1>
-        <h2 id="content">Some text here.</h2>
+      <div class="slide visible-xs">
+        <!--a part ends here-->
+        <div class="col-xs-12">
+          <li><h3 id="content">Massage</h3></li>
+          <div class="col-xs-4">
+            <a href="#" class="thumbnail">
+              <img src="img/img_massage.jpg" alt="...">
+            </a>
+          </div>
+          <div class="col-xs-8">
+            <p>
+              You can also try our latest Thermal Jade Massage beds with clothes on for: fatigue, to enhance immunity, sciatica, prostatitis, diabetes, muscle strain, leg pains, correction of the spine, cervical spondylosis, constipation, gastritis, and gynaecological diseases.
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-12">
+          <li><h3 id="content">​Reflexology</h3></li>
+          <div class="col-xs-4">
+            <a href="#" class="thumbnail">
+              <img src="img/img_reflexology.jpg" alt="...">
+            </a>
+          </div>
+          <div class="col-xs-8">
+            <p>
+              Please book in advance and  try our latest massages too.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="section" id="csscontact">
-      <div class="hidden-xs">
+      <div class="slide">
         <div class="col-sm-12">
-          <div class="col-sm-6" align="center">
+          <div class="col-sm-6 col-xs-12" align="center">
             <h2 id="title">About</h2>
             <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
               <div class="panel panel-default">
@@ -253,8 +328,41 @@
                 </div>
               </div>
             </div>
+            <div id="content" class="visible-xs">
+              <label><h3><----------slide to book</h3></label>
+            </div>
           </div>
-          <div class="col-sm-6">
+          <div class="visible-xs slide">
+            <h2 id="title">Book Your Appointments</h2>
+            <form class="" action="index.html" method="post">
+              <div class="col-xs-6">
+                <label for="">Name :</label>
+                <input type="text" class="form-control" name="name" value="" required>
+                <label for="">Phone Number :</label>
+                <input type="text" class="form-control" name="name" value="" required>
+                <label for="">Email :</label>
+                <input type="text" class="form-control" name="name" value="">
+              </div>
+              <div class="col-xs-6">
+                <label for="">Therapist :</label>
+                <select class="form-control" name="" required>
+                  <option>first</option>
+                </select>
+                <label for="">Date :</label>
+                <input type="date" class="form-control" name="name" value="">
+                <label for="">Time :</label>
+                <select class="form-control" name="" required>
+                  <option>first</option>
+                </select>
+              </div>
+              <div class="col-xs-12">
+                <label for="">Main Symptoms :</label>
+                <textarea name="name" class="form-control" rows="8" cols="40" required></textarea>
+                <input type="submit" class="btn btn-default" value="Submit">
+              </div>
+            </form>
+          </div>
+          <div class="col-sm-6 hidden-xs">
             <h2 id="title">Book Your Appointments</h2>
             <form class="" action="index.html" method="post">
               <div class="col-sm-6">
@@ -286,48 +394,7 @@
           </div>
         </div>
       </div>
-      <!--for cell phone version-->
-      <div class="visible-xs">
-        <div class="col-sm-12" align="center">
-          <h1 id="title">General Inquiries & Quotes:</h1>
-          <br>
-          <div class="col-sm-4 col-xs-12" align="center">
-            <button type="button" class="btn btn-default btn-lg btn-raised">
-              <span class="fa fa-phone fa-3x hidden-xs" aria-hidden="true"></span>
-              <h4>Tel : 1.403.617.9780</h4>
-            </button>
-          </div>
-          <div class="col-sm-4 col-xs-12" align="center">
-            <a href="mailto:info@smartconsult.ca" class="btn btn-default btn-lg btn-raised">
-              <i class="fa fa-envelope fa-3x hidden-xs"></i>
-              <h5 class="visible-xs">info@smartconsult.ca</h5>
-              <h3 class="hidden-xs">info@smartconsult.ca</h3>
-            </a>
-          </div>
-          <div class="col-sm-4 hidden-xs">
-            <a class="btn btn-lg btn-default btn-raised" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-              <span class="fa fa-globe fa-3x" aria-hidden="true"></span>
-              <h3>Location</h3>
-            </a>
-            <div class="collapse" id="collapseExample">
-              <div class="well">
-                70 Royal Oak Plaza NW Calgary AB
-                <iframe width="100%" height="300" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q=70%20Royal%20Oak%20Plaza%20NW%2C%20Calgary%2C%20AB%2C%20Canada&key=AIzaSyAFpllDDTKOIMHw7vJrebtQKbPifQ5pAxY" allowfullscreen></iframe>
-              </div>
-            </div>
-          </div>
-          <div class="col-xs-12 visible-xs">
-            <a class="btn btn-lg btn-default btn-raised" role="button" data-toggle="collapse" href="#collapseExample-xs" aria-expanded="false" aria-controls="collapseExample">
-              <h5>Location</h5>
-            </a>
-            <div class="collapse" id="collapseExample-xs">
-              <div class="well">
-                70 Royal Oak Plaza NW Calgary AB
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
     </div>
   </div>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -341,7 +408,8 @@
     $(document).ready(function() {
       $('#fullpage').fullpage({
         anchors:['home' ,'therapies', 'contact'],
-        menu: '#myMenu'
+        menu: '#myMenu',
+        controlArrows: false,
       });
     });
   </script>
